bcf2b68c8db7f69603558244032cfad0285c1549
Sajad Amiri <sajad.vb98@gmail.com>
2019-07-24 23:13:26 -0700

    event and emit by creting class and define emit in another module

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const EventEmitter = require('events');
 
-const emitter = new EventEmitter();
+const Logger = require('./logger');
+const logger = new Logger();
 
-emitter.on('messageLogged' , (arg)=> {
+logger.on('messageLogged' , (arg)=> {
     console.log('Listener called' , arg)
 })
 
-emitter.emit('messageLogged' , {id: 1 , url: 'http://'});
-
+logger.log('message');
--- logger.js
+++ logger.js
@@ -1,7 +1,13 @@
-var url = "http://mylogger.io/log";
 
-function log(message) {
+const EventEmitter = require('events');
+
+class Logger extends EventEmitter{
+    log(message) {
     console.log(message);
+
+    this.emit('messageLogged' , {id: 1 , url: 'http://'});
+
+}
 }
 
-module.exports = log;
+module.exports = Logger;
