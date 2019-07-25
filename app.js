9283878562541c95bdc506af319a8b7523642cdb
Sajad Amiri <sajad.vb98@gmail.com>
2019-07-24 22:32:07 -0700

    filesystems and different between Sync and Async methods

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const os = require('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+const files = fs.readdirSync('./');
+console.log(files);
 
-console.log(`TotalMemory: ${totalMemory}`);
-console.log(`FreeMemory: ${freeMemory}`);
+fs.readdir('./' , function (err ,files) {
+    if (err) console.log('Error' , err);
+    else console.log('Result' , files);
+});
